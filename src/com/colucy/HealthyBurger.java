327d303d67795b546a511993da7abb538a210870
Cindy Colucy <ckcolucy@gmail.com>
2017-12-26 14:46:47 -0500

    "Healthy Burger"

--- src/com/colucy/HealthyBurger.java
+++ src/com/colucy/HealthyBurger.java
@@ -1,6 +1,24 @@
 package com.colucy;
 
-//public class HealthyBurger extends Hamburger{
-//
-//
-//}
+public class HealthyBurger extends Hamburger {
+    private boolean sprouts;
+    private boolean carrots;
+
+    public HealthyBurger(String name, double price, String bread, String meat, boolean lettuce, boolean tomato, boolean onion, boolean mayo, boolean sprouts, boolean carrots) {
+        super("Healthy", price, "brown rye", meat, lettuce, tomato, onion, mayo);
+        this.sprouts = sprouts;
+        this.carrots = carrots;
+    }
+
+    @Override
+    public double burgerPrice() {
+        double hamburgerPrice = super.burgerPrice();
+        if (sprouts == true) {
+            hamburgerPrice += .50;
+        }
+        if (carrots == true) {
+            hamburgerPrice += +.50;
+        }
+        return hamburgerPrice;
+    }
+}
--- src/com/colucy/Main.java
+++ src/com/colucy/Main.java
@@ -7,6 +7,9 @@ public class Main {
         Hamburger hamburger = new Hamburger("Basic", 5.00, "Sausage", "Buffalo", true, true, true, true);
         double totalPrice = hamburger.burgerPrice();
         System.out.println("Total burger price is " + hamburger.burgerPrice());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Healthy", 6.50, "Brown rye", "Bacon", true, true, true, false, true, true);
+        System.out.println("Total burger price is " + healthyBurger.burgerPrice());
     }
 
 
